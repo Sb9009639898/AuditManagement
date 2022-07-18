d6fba655e59fc6b567e7d8b619d13abe0e42285a
Ilarika Rautela <khushirautela27@gmail.com>
2022-07-18 21:39:13 +0530

    First commit

---
+++ audit-authorization/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (1, 'Akshay Bhoi', 'ab7', 'ab1234', 'Project-1');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (2, 'Tazaeen Shaikh', 'taz7', 'ts1234', 'Project-2');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (3, 'Ankita Mane', 'anky7', 'am1234', 'Project-3');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (4, 'Ilarika Rautela', 'ilr7', 'ir1234', 'Project-4');
+INSERT INTO project_manager(id, name, username, password, project_name) VALUES (5, 'Shubham Chaturvedi', 'sc7', 'sc1234', 'Project-5');
